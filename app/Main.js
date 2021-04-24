bd6b583375386bd602ebedfbb53601f52a677dab
Orlando <orlandopablo.cr@gmail.com>
2021-04-24 19:02:43 -0400

    update: add client side renderind

--- app/Main.js
+++ app/Main.js
@@ -1,18 +1,31 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
 
 // My componenst
 import Header from "./components/Header"
 import HomeGuest from "./components/HomeGuest"
 import Footer from "./components/Footer"
+import About from "./components/About"
+import Terms from "./components/Terms"
 
 function Main() {
   return (
-    <>
-      <Header></Header>
-      <HomeGuest></HomeGuest>
-      <Footer></Footer>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/" exact>
+          <HomeGuest></HomeGuest>
+        </Route>
+        <Route path="/about-us" exact>
+          <About></About>
+        </Route>
+        <Route path="/terms" exact>
+          <Terms></Terms>
+        </Route>
+      </Switch>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
